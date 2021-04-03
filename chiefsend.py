945893083d403bfa0309365c7b3800e696d9daf2
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-03 07:21:24 +0200

    fix env variables, global API_URL

--- chiefsend.py
+++ chiefsend.py
@@ -1,7 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['API_URL'] = 'http://localhost:6969'
-app.config['SECRET_KEY'] = '0345896u409568540968630498649058609'
+app.config['API_URL'] = os.getenv("API_URL") or 'http://localhost:6969'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") or '0345896u409568540968630498649058609'
+
+app.jinja_env.globals['API_URL'] = app.config['API_URL']
 
 import routes
